e6d401a84b89f5997f574e3383832d75db210a30
QingshiLane <98565268+QingshiLane@users.noreply.github.com>
2024-08-18 18:15:07 +0800

    Update tg_bot_stop.sh

--- tg_bot_stop.sh
+++ tg_bot_stop.sh
@@ -13,10 +13,10 @@ PROCESS_NAME="python "$SCRIPT_DIR/homing_pigeon_bot.py" "$TOKEN" "$YOUR_USER_ID"
 if pgrep -f "$PROCESS_NAME" > /dev/null; then
     echo "进程 '$PROCESS_NAME' 在运行。"
     pkill -f "$PROCESS_NAME"
-    echo "进程 '$PROCESS_NAME' 已终止，机器人停止运行。"
+    echo "进程已终止，机器人停止运行。"
     sleep 1
 else
-    echo "进程 '$PROCESS_NAME' 不存在，机器人不在运行，无需停止。"
+    echo "进程不存在，机器人不在运行，无需停止。"
 
 fi
 
